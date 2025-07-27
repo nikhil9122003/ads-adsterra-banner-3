22668f86a5b6ba4f933f61e845109b243c05ff94
nikhil9122003 <166285467+nikhil9122003@users.noreply.github.com>
2025-07-27 11:48:55 +0530

    Update ads-160x300.js

--- ads-160x300.js
+++ ads-160x300.js
@@ -1,14 +1,28 @@
 (function() {
-  var atOptions = {
-    'key' : 'ef72795a8779ee018916983acc759db4',
-    'format' : 'iframe',
-    'height' : 300,
-    'width' : 160,
-    'params' : {}
-  };
-  var s = document.createElement('script');
-  s.type = 'text/javascript';
-  s.src = '//www.highperformanceformat.com/ef72795a8779ee018916983acc759db4/invoke.js';
-  s.async = true;
-  document.currentScript.parentNode.insertBefore(s, document.currentScript);
+  var container = document.getElementById("ad-160x300");
+
+  if (container) {
+    // Set Adsterra configuration
+    var configScript = document.createElement('script');
+    configScript.type = 'text/javascript';
+    configScript.innerHTML = `
+      atOptions = {
+        'key' : 'ef72795a8779ee018916983acc759db4',
+        'format' : 'iframe',
+        'height' : 300,
+        'width' : 160,
+        'params' : {}
+      };
+    `;
+    container.appendChild(configScript);
+
+    // Load the invoke.js script
+    var adScript = document.createElement('script');
+    adScript.type = 'text/javascript';
+    adScript.src = 'https://www.highperformanceformat.com/ef72795a8779ee018916983acc759db4/invoke.js';
+    adScript.async = true;
+    container.appendChild(adScript);
+  } else {
+    console.warn('Ad container #ad-160x300 not found.');
+  }
 })();
