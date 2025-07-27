296899dfe95d4201ddbfd6ef0f531b124594fc67
nikhil9122003 <166285467+nikhil9122003@users.noreply.github.com>
2025-07-27 11:12:00 +0530

    Create ads-160x300.js

---
+++ ads-160x300.js
@@ -0,0 +1,14 @@
+(function() {
+  var atOptions = {
+    'key' : 'ef72795a8779ee018916983acc759db4',
+    'format' : 'iframe',
+    'height' : 300,
+    'width' : 160,
+    'params' : {}
+  };
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.src = '//www.highperformanceformat.com/ef72795a8779ee018916983acc759db4/invoke.js';
+  s.async = true;
+  document.currentScript.parentNode.insertBefore(s, document.currentScript);
+})();
